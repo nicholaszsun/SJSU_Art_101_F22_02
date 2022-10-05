a3f4c4639d93e87bdb21c1b214f53b7aa897e722
Alicia Chi <100648260+AliciaYChi@users.noreply.github.com>
2022-10-04 17:27:46 -0700

    IDK

--- Assignments/alicia_chi/randomizer/avatar/sketch.js
+++ Assignments/alicia_chi/randomizer/avatar/sketch.js
@@ -7,16 +7,6 @@ var moves= .6;
 let tryOne;
 let tryTwo;
 let sketchStarted = false;
-let vol = mic.getLevel()*1.2;
-    let vol1 = mic.getLevel();
-    let vol_mouth = mic.getLevel()*-1.5;
-    let i= width/2;
-    let j= height/2;
-    let t= color(114, 91, 143);
-    let t1= color (76, 50, 89);
-    let h= color(255, 206, 122);
-    let e= color(57, 57, 66);
-    let b= color(255, 143, 165);
 
 //WAH!
 function setup() {
@@ -55,11 +45,20 @@ function setup() {
 
 function draw() {
   background(233, 230, 252);
+  let vol = mic.getLevel()*1.2;
+  let vol1 = mic.getLevel();
+  let vol_mouth = mic.getLevel()*-1.5;
+  let i= width/2;
+  let j= height/2;
+  let t= color(114, 91, 143);
+  let t1= color (76, 50, 89);
+  let h= color(255, 206, 122);
+  let e= color(57, 57, 66);
+  let b= color(255, 143, 165);
 
  
 
   if(sketchStarted){
-    
 
     for (let i = 0; i < confetti.length / 2; i++) {
       confetti[i].confettiDisplay();
