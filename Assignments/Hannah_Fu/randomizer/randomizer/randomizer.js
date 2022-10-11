1fb5c2d73b174fa3018ced39a792867f1eac4532
fictionalspace <hannah.fu@sjsu.edu>
2022-10-11 15:51:29 -0700

    Summoning randomizer at  2b.2

---
+++ Assignments/Hannah_Fu/randomizer/randomizer/randomizer.js
@@ -0,0 +1,13 @@
+let dogs = ["Holly", "Polly", "Molly", "Abby", "Gabby", "Jabby"];
+
+function setup() {
+    createCanvas(600, 600);
+    background(200);
+
+    dogs[2] = "Polly Popper";
+    console.log(dogs);
+}
+
+function draw (){
+
+}
