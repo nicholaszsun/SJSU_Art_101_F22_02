ac4dad103b5f984ba87c648b25b96c3f81d9d549
Alicia Chi <100648260+AliciaYChi@users.noreply.github.com>
2022-10-11 16:54:36 -0700

    Randomizer

--- Assignments/alicia_chi/Node 2b.4/sketch.js
+++ Assignments/alicia_chi/Node 2b.4/sketch.js
@@ -1,3 +1,4 @@
+
 let feelings = [{
     name: "Sarrow", 
     color: "blue"
@@ -5,32 +6,62 @@ let feelings = [{
     name:"Anger", 
     color: "red"
 }, {
-    name:"Annoyance", color: "purple"
+    name:"Annoyance", 
+    color: "purple"
 }, {
-    name:"Happiness", color: "yellow"
+    name:"Happiness",
+     color: "yellow"
 }, {
-    name: "Disgust", color:"green"
+    name: "Disgust", 
+    color:"green"
 }, {
-    name:"Neutral", color:"grey"
+    name:"Neutral", 
+    color:"grey"
 }];
+
+let randomIndex;
+let animating = false;
 
 function setup(){
     createCanvas (600, 600);
     background(200);
+    textSize(18);
+    text("click to randomize", 50, 50);
+   
+   
 }
 
 function draw(){
     
+    if(animating==true){
+        triangle(random(600),random(600), random(600), random(600), random(600),random(600));
+    }
    
 }
 
-function mousePressed(){
+function randomizer(){
+    animating=false;
+
+    if(feelings[0]){
+
     background(random(255));
-randomIndex = int(random(feelings.length));
+    randomIndex = int(random(feelings.length));
+    console.log(feelings[randomIndex].name);
+    text(`The color ${feelings[randomIndex].name} associated is ${feelings[randomIndex].color}.`, 50,50);
+    //text(feelings[randomIndex].name + "'s associated color is " + feelings[randomIndex].color, 50,50);
+    feelings.splice(randomIndex, 1);
+   
+}else{
+    background(random(255));
+    text("Nothing Left!", 50, 50);
 
-   console.log(feelings[randomIndex].name);
-   text(feelings[randomIndex].name, 100,100);
+}
+}
 
-   feelings.splice(randomIndex, 1);
-   console.log(feelings);
+function mousePressed(){
+animating = true;
+setTimeout(randomizer, 2000);
+
+
+
 }
