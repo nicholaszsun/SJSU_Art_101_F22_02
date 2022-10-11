43703eb880321adc4a3538a531152946c4f8ecc0
fictionalspace <hannah.fu@sjsu.edu>
2022-10-11 16:49:14 -0700

    randomizer 2b.3

--- Assignments/Hannah_Fu/randomizer/randomizer/sketch.js
+++ Assignments/Hannah_Fu/randomizer/randomizer/sketch.js
@@ -1,17 +1,24 @@
-let dogs = ["Holly", "Polly", "Molly", "Abby", "Gabby", "Jabby"];
+let flowers = [
+{name:"myosotis", 
+color: "blue"}, 
+
+{name:"rosemary", 
+color: "toad green"},
+
+{name:"lavender", 
+color: "light violet"},
+
+{name: "marigold", 
+color: "bright yellow"},
+
+{name: "snowdrop", 
+color: "snowy white"}];
 
 function setup() {
     createCanvas(600, 600);
     background(200);
-    //dogs[0] = "Holly Healer";
-    //dogs[1] = "Polly Popper";
-
-    console.log("initial array is")
-    console.log(dogs);
-
-    dogs.splice(4,1);
-    console.log("array after splice")
-    console.log(dogs);
+   
+    console.log(flowers[0].color);
  
 
 }
