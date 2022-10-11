bbd72d2631f32c01ca1819aa82690c539d332fde
Alicia Chi <100648260+AliciaYChi@users.noreply.github.com>
2022-10-10 22:24:15 -0700

    randomizer

---
+++ Assignments/alicia_chi/randomizer/sketch.js
@@ -0,0 +1,18 @@
+let feelings = ["Sarrow", "Anger", "Annoyance", "Happiness", "Disgust", "Neutral"]
+
+function setup(){
+    createCanvas (600, 600);
+    background(200);
+
+    feelings[1] = "Happiness";
+    feelings[2] = "Anger";
+    feelings[3] = "Annoyance";
+    feelings[4] = "Happiness";
+    feelings[5] = "Disgust";
+    feelings[6] = "Neutral";
+    console.log(feelings);
+}
+
+function draw(){
+    
+}
