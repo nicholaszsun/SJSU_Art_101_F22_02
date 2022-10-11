726a3864cad36a6d713ffd454187b8f05bb32e68
smunoz408 <72097939+smunoz408@users.noreply.github.com>
2022-10-11 13:47:09 -0700

    Update sketch.js

--- Assignments/Serena_Munoz/Randomizer_2/sketch.js
+++ Assignments/Serena_Munoz/Randomizer_2/sketch.js
@@ -1,99 +1,41 @@
-let mic;
-let y = 0;
-let x=0;
-let count = 0;
-let myMap=0;
-let hight=0;
+let input, button, greeting;
 
 function setup() {
- //background(0);
- let cnv = createCanvas(400, 400);
-cnv.mousePressed(userStartAudio);
-mic = new p5.AudioIn();
-  mic.start();
-}
-function draw() {
- //background(0);
- light();
- hair();
- face();
- eyes();
- eyebrows();
- lips();
-  
+  // create canvas
+ createCanvas(400, 400);
+	
 
-}
-function hair(){
-  push();
-  //Hair 
-  fill(144, 82, 163);
-    ellipse(36,386, 110, 155);
-      ellipse(86,150, 110, 155);
-      ellipse(50,260, 110, 155);
-    ellipse(216,106, 200, 155);
-    ellipse(286,170, 200, 155);
-    ellipse(340,286, 110, 155);
+  input = createInput();
+  input.position(20, 65);
 
-rect(20, 70, 350, 590, 200);
-  pop();
+  button = createButton('submit');
+  button.position(input.x + input.width, 65);
+  button.mousePressed(greet);
+
+  greeting = createElement('h2', 'what is your name?');
+  greeting.position(50, 5);
+
+  textAlign(CENTER);
+  textSize(100);
+}
+function draw(){
+  //greet()
+ // background (random(255), random(244), random(244));
   
 }
-function face(){
-  fill(216, 145, 108);
+function greet() {
+  const name = input.value();
 
-rect(50,100, 290, 370, 300);
-}
-function eyes(){
-  //eye1
-        fill(213, 137, 11);
-   ellipse(130,270,56,40);
+  greeting.html('hello ' + name + '!');
 
- //eye2
-      
-   fill(213, 137, 11 );
-  ellipse(275,259,56,40);
- 
-    }
-function eyebrows(){
-    // eyebrow
-    beginShape();
-    fill(179, 50, 74 );
-  translate(width/1.5,height/1.8);
-  vertex(-70,0);
-  bezierVertex(-mouseX,-30,20,-mouseX,80,0);
-  bezierVertex(-30,-50,-30,-100,-80,0)
-     bezierVertex(30,50,30,100,80,0)
+  input.value('');
 
-  endShape();
-  // eyebrow
-  beginShape();
-     translate(width/-2.9,height/25);
-
-  vertex(-70,0);
-   bezierVertex(-mouseX,-30,20,-mouseX,80,0);
-  bezierVertex(-30,-50,-30,-100,-80,0)
-   bezierVertex(30,50,30,100,80,0)
-  
-  endShape();  
-}
-function lips(){
-  translate(10,0);
-
-arc(90, 130, 70, 80, 0.2, PI * QUARTER_PI, PIE);
-   
-}
-function light(){
-
-    fill(random(0, 255),(0, 0), (0, 255) );
-  micLevel = mic.getLevel();
-  let  = height - micLevel * height;
-  let x = hight + micLevel * height;
-   noStroke();
-   ellipse(100,260/width, x, 200);
-   ellipse(260,260/width, x, 700);
-   ellipse(360,260/width, x, 700);
-     ellipse(180,260/width, x, 700);
-
-   ellipse(5,260/width, x, 500);
-
+  for (let i = 0; i < 20; i++) {
+    push();
+    fill(random(255), random(244), random(244));
+    translate(random(width),(height));
+    rotate(random(2* PI));
+    text(name, 100, 0);
+    pop();
+  }
 }
