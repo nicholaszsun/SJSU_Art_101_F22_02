e35ac722064d90cd94e69fe8a50a040e3b36613e
fictionalspace <hannah.fu@sjsu.edu>
2022-10-12 18:43:31 -0700

    2b.8 Randomizer

--- Assignments/Hannah_Fu/randomizer/randomizer/sketch.js
+++ Assignments/Hannah_Fu/randomizer/randomizer/sketch.js
@@ -22,7 +22,8 @@ color: "ruby red"}];
 
 
 let randomIndex;
-
+let animating = false;
+//let counter = 0;
 function setup() {
     //let r = random(6);
     createCanvas(600, 600);
@@ -34,16 +35,29 @@ function setup() {
 
 function draw (){
 
+    if(animating == true){
+        ellipse(random(width), random(height), random(50, 200));
+    }
 }
 
-function mousePressed(){
+    
+function randomizer(){
+    animating = false;
+
 if (flowers[0]){
     background(random(200, 255));
     randomIndex = int(random(flowers.length));
-    text(flowers[randomIndex].name, 50, 50);
+    text(`${flowers[randomIndex].name}'s color is 
+     ${flowers[randomIndex].color}`, 50, 50);
     flowers.splice(randomIndex, 1);
 } else {
     background(random(200, 255));
     text("nothing left!", 50, 50);
 }
+}
+function mousePressed(){
+    animating = true;
+    setTimeout(randomizer, 2000);
+
+
 }
