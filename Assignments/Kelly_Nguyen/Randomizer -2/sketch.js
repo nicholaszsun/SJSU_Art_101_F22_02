e11a8ed8274d6c9ca36e830bdaae0b3d78757d03
KNKelly <kelly.h.nguyen01@sjsu.edu>
2022-10-18 17:25:31 -0700

    updating

--- Assignments/Kelly_Nguyen/Randomizer -2/sketch.js
+++ Assignments/Kelly_Nguyen/Randomizer -2/sketch.js
@@ -1,69 +1,66 @@
-let things = [{
-    name:"3ae",
-    color: "3ac"
-}, {
-    name:"xanh",
-    color:"3xa"
-}, {
-    name: "mi",
-    color:"3mi"
-}, {
-    name:"hom",
-    color:"3hom"
-}, {
-    name:"che",
-    color:"3che"
-}, {
-    name:"bot",
-    color:"3bot"
-}];
 
 let randomIndex;
 let counter = 0;
 let cheddar = false;
+let button;
+let j;
+
+let cats = [];
+
+function preload(){
+    for (let i =0; i <= 15; i++){
+        cats[i] = loadImage('assets/cat_'+i+'.jpg')
+    }
+}
 
 function setup (){
     createCanvas(windowWidth,windowHeight);
     background(200);
-
-    
-text("Click to see how you feel today", 50,50);
+// text("ClickClack", 50,50);
 // setInterval(changeBackground, 1000);
 //set time out is expecting a function (change background is the function in this
 //case)
+    button = createButton('click me');
+    button.position(50,50);
+    button.mousePressed(buttonPressed);
+    console.log(cats);
 }
 
 function draw(){
-
     if(cheddar== true){
-        ellipse(random(width),random(height),random(50,100))
+        let k = int(random(14))
+        image(cats[k],0,0);
     }
 
 }
+
+
 function changeBackground(){
     if (counter <=5){
         counter++;
-    
-    background(random(255),random(255),random(255));
- } else {
-
- }
+        background(random(255),random(255),random(255));
+        } else {
+    }
 }
+
+
 function loading(){
-    if(things[0]){
-    background(random(255));
-    randomIndex = int(random(things.length));
-    text(things[randomIndex].name,50,50);
-    things.splice(randomIndex, 1);
-} else{
-    background(255)
-    text("STOP CLICKING FAST",50,50)
+    if(cats[14]){
+    //background(random(255));
+    randomIndex = int(random(cats.length));
+    // text(things[randomIndex].name,50,50);
+    cats.splice(randomIndex, 1);
+    } else{
+        background(255)
+        text("STOP CLICKING FAST",50,50)
+    }
 }
-}
-function mousePressed(){
+
+function buttonPressed(){
     cheddar = true;
-    setTimeout(loading,2000);
-
-
-   
+    j = int(random(15));
+    setTimeout(loading,10);
+    console.log(j);
 }
+
+//myclass into array -> my array dot one as an element of array n call as random
